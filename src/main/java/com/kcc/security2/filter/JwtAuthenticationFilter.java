34fbf7f23cb90e0d1e27c54f4f0f9be3f4ad34b6
haha002024 <haha002024@gmail.com>
2024-08-26 14:26:22 +0900

    중간저장

--- src/main/java/com/kcc/security2/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/kcc/security2/filter/JwtAuthenticationFilter.java
@@ -1,13 +1,20 @@
 package com.kcc.security2.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kcc.security2.model.User;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import java.io.IOException;
+import java.security.Principal;
+
+// 로그인을 요청 했을때 실행된다.
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     //AuthenticationManager 은 정해진것이다.
@@ -16,8 +23,32 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
         System.out.println("attemtAuthentication 호출");
-        return super.attemptAuthentication(request, response);
 
+        //1. username , password 받기
+        try{
+            ObjectMapper om = new ObjectMapper();// json데이처를 파싱
+            // request.getInputStream() 요청받은 아이디와 패스워드를 받아서 읽어서 user 객체에 넣어줌
+            User user = om.readValue(request.getInputStream(), User.class);
+            System.out.println(user);
+
+            // username과 password를 넣어줌
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword());
+
+            // principalDetailService loadUserByUsername()을 호출해줘야함
+            //authenticate(authenticationToken) : username을 전달
+            // principal detail에 전달함
+            Authentication authentication = authenticationManager.authenticate(authenticationToken);
+
+            Principal principal = (Principal) authentication.getPrincipal();
+            return  authentication;
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+
+        return null;
     }
 
 }
